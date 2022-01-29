dfad856c62437806a67d75041252cc9c52f169e7
TimurMamedov-zz <tima_mamedxp@mail.ru>
2022-01-29 22:25:03 +0300

    [hw-core-python] first task

--- ex1/__init__.py
+++ ex1/__init__.py
@@ -1,7 +1,15 @@
 def build_roles_tree(mapping):
-    """
-    :param mapping: маппинг ролей в категории
-    :return: дерево ролей
-    """
-    # put your code here
-    pass
+    roles = mapping['roles']
+    categories = mapping['categories']
+    category_ids_sorted = mapping['categoryIdsSorted']
+    result_tree = {'categories': []}
+    result_list = result_tree['categories']
+    for categoryId in category_ids_sorted:
+        category = categories[categoryId]
+        role_ids = category['roleIds']
+        result_category = {'id': 'category-' + categoryId, 'text': category['name'], 'items': []}
+        items = result_category['items']
+        for roleId in role_ids:
+            items.append({'id': roleId, 'text': roles[roleId]['name']})
+        result_list.append(result_category)
+    return result_tree
