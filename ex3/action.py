fc0da476821b185ed58b34a33e38b3886d11d143
TimurMamedov-zz <tima_mamedxp@mail.ru>
2022-01-30 16:27:38 +0300

    [hw-core-python] third task

--- ex3/action.py
+++ ex3/action.py
@@ -1,20 +1,57 @@
 class BaseAction:
     def __init__(self, name):
         self.name = name
+        self.hash = hash(self.name)
 
     def __repr__(self):
         return self.name
 
+    def __hash__(self):
+        return self.hash
+
+    def __eq__(self, other):
+        if self.hash != other.hash:
+            return False
+        return self.name == other.name
+
+    def __ne__(self, other):
+        return not self == other
+
+    def __lt__(self, other):
+        if self == other:
+            return False
+        if self.name == 'Nothing':
+            return True
+        if self.name == 'Rock' and other.name == 'Paper':
+            return True
+        if self.name == 'Paper' and other.name == 'Scissors':
+            return True
+        if self.name == 'Scissors' and other.name == 'Rock':
+            return True
+        return False
+
+    def __le__(self, other):
+        if self == other:
+            return True
+        return self < other
+
+    def __gt__(self, other):
+        if self == other:
+            return False
+        return other < self
+
+    def __ge__(self, other):
+        if self == other:
+            return True
+        return self > other
 
 class NothingAction(BaseAction):
     def __init__(self):
         super().__init__('Nothing')
 
-
 class RockAction(BaseAction):
     def __init__(self):
         super().__init__('Rock')
-
 
 class PaperAction(BaseAction):
     def __init__(self):
